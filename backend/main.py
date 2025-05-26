a0847262c2e8c1e5d94e437da56323938d09d163
Anne-Esther <144198087+Anne-Esther@users.noreply.github.com>
2025-05-26 04:05:50 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import predict
 import os
 import uvicorn
 
+app = FastAPI()
+
+# CORS settings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include routes
+app.include_router(predict.router)
+
+# Lancement si exécuté directement
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
