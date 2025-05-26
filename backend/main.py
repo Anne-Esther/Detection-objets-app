cf4e4038e740be341de2a733fd2678e6517738bf
Anne-Esther <144198087+Anne-Esther@users.noreply.github.com>
2025-05-26 03:05:55 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,6 @@
-from yolo import load_model
-model = load_model()
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
